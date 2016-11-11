b4b7c0537ba6d924b6e2432f4f318fdacd2b6bb9
David Quail <quail.david@gmail.com>
2016-11-11 12:38:42 -0800

    Part 1 of assignment

--- Tilecoder.py
+++ Tilecoder.py
@@ -1,7 +1,23 @@
 numTilings = 8
-    
+minInput = 0
+maxInput = 6
+tilesPerTiling = 11
+tileSize = (maxInput-minInput) / (tilesPerTiling -1) #0.6
+tilingOffset = tileSize / numTilings #0.6/8
+
 def tilecode(in1,in2,tileIndices):
     # write your tilecoder here (5 lines or so)
+
+    for tiling in range(numTilings):
+        print("Passing through tiling: " + str(tiling))
+        x = int(in1/tileSize)
+        y = int(in2/tileSize)
+        index = (y*tilesPerTiling + x) + tiling*tilesPerTiling*tilesPerTiling
+        tileIndices[tiling] = index
+        in1+=tilingOffset
+        in2+=tilingOffset
+        print(index)
+        
     
     
 def printTileCoderIndices(in1,in2):
@@ -13,4 +29,4 @@ printTileCoderIndices(0.1,0.1)
 printTileCoderIndices(4.0,2.0)
 printTileCoderIndices(5.99,5.99)
 printTileCoderIndices(4.0,2.1)
-    
+
